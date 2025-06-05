ceb99955098250818314beea6c1feef715493f2a
SEGUNDO ANDRES SHIGUI LEMA <sa.shiguil@uea.edu.ec>
2025-06-05 12:46:50 -0500

    Semana 02 - Tarea: Cálculo de área y el perímetro de figuras geométricas

--- Semana_02/Program.cs
+++ Semana_02/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 /// <summary>
 /// Clase que representa un Rectángulo y encapsula sus propiedades primitivas
 /// </summary>
